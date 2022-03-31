f22892faa51bd3630268252f703b75771c42f1c6
pooja <pooja>
2022-03-31 15:27:32 +0000

    Add Substraction Function

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,7 @@ public class Calculator
 public static void main(String args[])   
 {  
    sum(20,30);
+   sub(30,15);
  
 }
 
@@ -13,4 +14,12 @@ public static void sum(int n1, int n2){
 
     System.out.println("The sum of numbers is: "+sum); 
  }
+  public static void sub(int n1, int n2){
+    int sub;
+    sub = n1-n2;  
+
+
+    System.out.println("The subtraction of numbers is: "+sub); 
+ }
+
 }

e6c39ca8070baf0835afaa0a9b2d388e59b9a196
pooja <pooja>
2022-03-31 15:25:07 +0000

    Add Summation Function

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator
+{
+public static void main(String args[])   
+{  
+   sum(20,30);
+ 
+}
+
+public static void sum(int n1, int n2){
+    int sum;
+    sum = n1 + n2;  
+
+
+    System.out.println("The sum of numbers is: "+sum); 
+ }
+}
